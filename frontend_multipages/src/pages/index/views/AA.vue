854f3213991e739262aad085eb50fb33293b1fa2
Mandy22376 <2297584887@qq.com>
2024-07-08 09:24:13 +0800

    abc

--- frontend_multipages/src/pages/index/views/AA.vue
+++ frontend_multipages/src/pages/index/views/AA.vue
@@ -22,7 +22,7 @@ const getUserInfo = ()=> {
 </script>
 
 <template>
-  <h1>This is an AA page</h1>
+  <h1>This is an AA page页面</h1>
   <button @click="gotoHome"> 返回主页 </button>&nbsp
   <el-input style="width: 200px" v-model="stt.username" placeholder="点击获取文件名"/>&nbsp
   <el-button type="primary" @click="getUserInfo">获取用户名</el-button>
